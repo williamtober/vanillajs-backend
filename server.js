747a374bf69ad34e6c468038004b5d219931fe66
William Tober <45222799+williamtober@users.noreply.github.com>
2022-09-22 15:13:46 -0400

    runtime based logging of session.. should be easy to log this to other sources now that the logics written.

--- server.js
+++ server.js
@@ -1,27 +1,47 @@
-const https = require('https');
+const http = require('http');
 
 const state = {
     // global state
     time : null,
-    error : [
-        // exampel of incident
-        // { code: 404, message: 'Not Found', timestamp : null, endpoint : null },
-    ],
+    err : {
+        get : {
+            // exmaple
+            // "/notrealpath" :  { code: 404, message: 'Not Found', timestamp : new Date()},
+        }, 
+        post : {
+
+        },
+        put : {
+
+        },
+        del : {
+
+        }
+    },
+
+    // state functions
+    // RHTML = RESPOND HTML
+    rhtml : (res, body) => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(body);
+    }
 
 }
 
-const server = https.createServer((req, res) => {
+
+const server = http.createServer((req, res) => {
 
     // get the request method and forward it to the appropriate function
     const method = req.method.toLowerCase();
-    
+
     switch(method) {
         case 'get': {
             // handle get request
-            const response = get(req);
-            res.writeHead(200, response.head);
-            res.end(response.body);
-
+            get(req ,res);
+                
+            
+            
             break;
         } case 'post': {
             // handle post request
@@ -45,32 +65,44 @@ server.listen(3000, async () => {
     setInterval(() => {
         state.time = new Date();
         console.clear();
-        console.log(`Server is listening on port 3000.  [${state.time.toLocaleTimeString()}]`);
+        console.log(`Server is listening on port 3000.  [${state.time.toLocaleTimeString()}]\n `);
+        console.log(`ERRORS \n    GET`);
+        Object.keys(state.err.get).forEach((key) => {
+            console.log(`        ${key} : ${state.err.get[key].code} ${state.err.get[key].message} [${state.err.get[key].timestamp.toLocaleTimeString()}]`);
+        });
+
+
         
     }, 1000);
 });
 
 // processes get requets
-const get = (req) => {
+const get = async (req, res) => {
     switch(req.url) {
         case '/': {
-            return { head : { 'Content-Type': 'text/html' }, body: '<h1>Hello World!</h1>' };
+            state.rhtml(res, '<html><body><h1>Hello World</h1></body></html>');
+            break;
+        } default : {
+            state.err.get = {...state.err.get, [req.url] : { code: 404, message: 'Not Found', timestamp : new Date()}};
+            state.rhtml(res, '<html><body><h1>Not Found</h1></body></html>');
+            break;
         }
     }
-    return req.method === 'GET';
 }
+
+
 
 // process post requests
 const post = (req) => {
-    return req.method === 'POST';
+    return req.method === 'POST'
 }
 
 // process put requests
 const PUT = (req) => {
-    return req.method === 'PUT';
+    return req.method === 'PUT'
 }
 
 // process delete requests
 const del = (req) => {
-    return req.method === 'DELETE';
+    return req.method === 'DELETE'
 }
